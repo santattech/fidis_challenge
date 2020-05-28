2e3faea6ddc6340443575bcec09b66a9d0586b1b
Santanu Bhattacharya <santattech@gmail.com>
2020-05-28 22:47:30 +0530

    password validation added

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,4 +6,14 @@ class User < ApplicationRecord
   validates :email, format: { with: URI::MailTo::EMAIL_REGEXP }
   validates_uniqueness_of  :email, case_sensitive: false
   validates :password, presence: true, confirmation: true, if: proc { password_digest_changed?  }
+  validate :password_type
+
+  protected
+  def password_type
+    return if self.password.blank?
+
+    unless match_data = /^(?=.*?[A-Z])(?=.*?[a-z])(?=.*?[0-9])(?=.*?[#?!@$%^&*-]).{8}$/i.match(self.password)
+      errors.add(:password, "only 8 digits allowed, should contain at least one number, one special character, one capital letter, one small letter")
+    end
+  end
 end
